bd964138a3759d9afbf50e7f21a144f0e9325093
jeanieandie <77466070+jeanieandie@users.noreply.github.com>
2025-05-21 13:56:39 -0700

    dailies

--- src/views/Dailies.vue
+++ src/views/Dailies.vue
@@ -5,7 +5,59 @@ defineOptions({
 });
 </script>
 <template>
-<h1>gotta get git</h1>
-<h2>Patreon</h2>
-<h3>27 Reasons Why</h3>
+  <h1 class="text-9xl">dailies</h1>
+<div class="grid grid-cols-[repeat(3,minmax(0,1fr))] w-full h-6 gap-2 p-5">
+
+  <div class="grid grid-cols-[repeat(3,minmax(0,1fr))] w-full h-6 gap-2 p-5">
+  
+    <div class="bg-yellow-200 cursor-pointer hover:opacity-50 text-violet-900 text-center flex items-center justify-center text-xl" >
+      <br>🧬<br>blood sugar<br><br></div>
+    <div class="bg-rose-200 cursor-pointer hover:opacity-50 text-violet-900 text-center flex items-center justify-center text-xl" >
+      💉<br>insulin
+    </div>
+    <div class="bg-indigo-200 cursor-pointer hover:opacity-50 text-violet-900 text-center flex items-center justify-center text-xl" >
+      🔵<br>㏒
+    </div>
+  <br><br><br><br>  <br><br><br><br>
+  </div>
+
+  <div class="border border-rose-200 cursor-pointer hover:opacity-50 text-center flex items-center justify-center text-xl" >
+    <div>
+      <pre> why aren't these ol? </pre>
+      <ol>
+        <li><h1>gotta get git</h1></li>
+        <li><h2>Patreon</h2></li>
+        <li><h3>27 Reasons Why</h3></li>
+      </ol>
+    </div>
+  </div>
+
+  <div class="border border-indigo-200 cursor-pointer hover:opacity-50 text-center text-xl" >
+      <div>🏠home</div><br>
+      <div>🧭orientation</div><br>
+      <div>⚓anchor+git</div><br>
+      <div>⏰dailies</div><br>
+      <div>🤴royalwe</div><br>
+      <div>📆monthlies</div><br>
+      <div>🏰forecastle</div><br>
+      <div>📅yearlies</div><br>
+      <div>✨perfection</div><br>
+      <div>㏒</div><br>
+      <div>📚library</div><br>
+      <div>🗃️</div><br>
+      <div>👸exhibits</div><br>
+      <div>📁</div><br>
+      <div>📖vocablulary</div><br>
+      <div>✅check</div><br>
+      <div>👑royalwethepeople</div><br>
+  </div>
+  
+  
+  <br><br><br><br>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+</div>  
+
+  <br><br><br><br>  <br><br><br><br>  <br><br><br><br>  <br><br><br><br>  <br><br><br><br>  <br><br><br><br>  <br><br><br><br>
+<br><br><br><br><br><br><br><br><br><br><br><br>
 </template>
--- src/views/Orientation.vue
+++ src/views/Orientation.vue
@@ -7,5 +7,34 @@ defineOptions({
 });
 </script>
 <template>
-
+<center class="text-9xl">🧭</center>
+<div>🏠<br>
+   home<br>
+   sea of Nought<br>
+   NaughtSea<br>
+   Vanity<br>
+   Meaningless<br>
+   nothing<br>
+   ∅©⾈<br>
+   ⛵
+  </div>
+  <div class="border border-amber-500 cursor-pointer hover:opacity-30 text-left text-xs" >
+    <pre>🏠home</pre>
+    <pre>🧭orientation</pre>
+    <pre>⚓anchor+git</pre>
+    <pre>⏰dailies</pre>
+    <pre>🤴royalwe</pre>
+    <pre>📆monthlies</pre>
+    <pre>🏰forecastle</pre>
+    <pre>📅yearlies</pre>
+    <pre>✨perfection</pre>
+    <pre>㏒</pre>
+    <pre>📚library</pre>
+    <pre>🗃️</pre>
+    <pre>👸exhibits</pre>
+    <pre>📁</pre>
+    <pre>📖vocablulary</pre>
+    <pre>✅check</pre>
+    <pre>👑royalwethepeople</pre>
+  </div>
 </template>
