0489aef5cae023ee5fb47eaf5d62d30c09cee5a5
jeanieandie <77466070+jeanieandie@users.noreply.github.com>
2025-05-23 19:03:36 -0700

    working but not pretty

--- src/App.vue
+++ src/App.vue
@@ -11,66 +11,62 @@ import TailwindColorNameBar from "./components/color/tailwindColorNameBar.vue";
 </script>
 <template>
 <backgroundImages>
-<div class="grid grid-cols-[repeat(3,minmax(0,1fr))] w-full h-6 gap-2 p-5">
+<div class="grid grid-cols-[repeat(5,minmax(0,1fr))] w-full h-6 gap-2 p-5">
+  <div>
+    <pre>10k</pre>
+  </div>
   <div class="border border-sky-300" >
-    <img src="/color/60 hsl wheel colors 1k2 opacity 100.png" class="w-100 object-cover rounded-md" />
+    <img src="/color/30 hsl wheel 10k2 opacity 100.png" class="w-100 object-cover rounded-md" />
   </div>
-  <div class="border border-sky-300">
-    <img src="/color/30 hsl wheel-0red 1k.png" class="w-100 object-cover rounded-md" />
+  <div class="border border-sky-300" >
+    <img src="/color/30 hsl wheel 10k2 opacity 100.png" class="w-100 object-cover rounded-md" />
   </div>
-  <div class="border border-sky-300">
-    <img src="/color/30 hsl wheel-0red 100.png" class="w-100 object-cover rounded-md" />
+  <div class="border border-sky-300" >
+    <img src="/color/30 hsl wheel 10k2 opacity 100.png" class="w-100 object-cover rounded-md" />
   </div>
-  <div class="border border-sky-300"><clock /></div>
+  <div class="border border-sky-300" >
+    <img src="/color/30 hsl wheel 10k2 opacity 100.png" class="w-100 object-cover rounded-md" />
+  </div>
 </div>
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 
-
+<div class="grid grid-cols-[repeat(5,minmax(0,1fr))] w-full h-6 gap-2 p-5">
+  <div>
+    <pre>10k</pre>
+  </div>
+  <div class="border border-sky-300" >
+    <img src="/color/60 hsl wheel 10k2 opacity 100.png" class="w-100 object-cover rounded-md" />
+  </div>
+  <div class="border border-sky-300" >
+    <img src="/color/60 hsl wheel 10k2 opacity 100.png" class="w-100 object-cover rounded-md" />
+  </div>
+  <div class="border border-sky-300" >
+    <img src="/color/60 hsl wheel 10k2 opacity 100.png" class="w-100 object-cover rounded-md" />
+  </div>
+  <div class="border border-sky-300" >
+    <img src="/color/60 hsl wheel 10k2 opacity 100.png" class="w-100 object-cover rounded-md" />
+  </div>
+</div>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 <hsl60Bar />
 
+<NavigationBar />  
 
-  <center>
-    
-              <NavigationBar />  
-  </center>
-<div class="grid grid-cols-[repeat(3,minmax(0,1fr))] w-full h-6 gap-2 p-5">
-  <div class="border border-sky-300" ><br><br><br><br><br><br><br><br></div>
-  <div class="border border-sky-300"></div>
-  <div class="border border-sky-300"></div>
+
+
+<br><br><hr><br><hr><br>
+
+<skyTop /><skyTop /><skyTop /><skyTop /><br>
+
+<div class="text-xl"><skyTop /><skyTop /><br>
 </div>
-<br><br><br><br><br><br><br><br><br><br><br>
-  <br><hr><br><hr><br><hr><br><hr><br><hr><br><br><br><br><hr><br><hr><br>  <br><br><hr><br><hr><br>  <br><br><hr><br><hr><br>
+<span  class="text-4xl">ن🖵無 </span><br>
+<RouterView />
+<span  class="text-4xl">無🖵ن </span><br>
+<span class="text-2xl"><skyBottom /><skyBottom /></span> 
 
-<div class="grid grid-cols-[repeat(3,minmax(0,1fr))] w-full h-6 gap-2 p-5">
-  <div class="bg-yellow-200 cursor-pointer hover:opacity-50 text-violet-800 text-center flex items-center justify-center text-5xl" ><br>🧬<br>blood sugar<br><br></div>
-  <div class="bg-rose-200 cursor-pointer hover:opacity-50 text-violet-800 text-center flex items-center justify-center text-5xl" >💉<br>insulin</div>
-  <div class="bg-indigo-200 cursor-pointer hover:opacity-50 text-violet-800 text-center flex items-center justify-center text-5xl" >🔵<br>㏒</div>
-</div>
-
-            <br><br><hr><br><hr><br>
-  <center>
-            <skyTop /><skyTop /><skyTop /><skyTop /><br>
-  </center>
-  <center class="text-xl"><skyTop /><skyTop /><br>
-
-            <!--<img src="/color/30 hsl wheel-0red 1k.png" alt="Example" class="w-1000 object-cover rounded-md" />-->
-            <!--<img src="/color/30 hsl wheel-0red 10k.png" alt="Example" class="w-10000 object-cover rounded-md" />
-            -->
-              <span  class="text-4xl">ن🖵無 </span><br>
-  <RouterView />
-            <span  class="text-4xl">無🖵ن </span><br>
-            <span class="text-2xl"><skyBottom /><skyBottom /></span> 
-  </center>
-            <br><br>
-  <center>
-  <routerBar />
-  <br><br>
-  <TailwindColorNameBar />
-  </center>
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-
+<routerBar />
+<TailwindColorNameBar />
 </backgroundImages>
 
 
@@ -78,62 +74,62 @@ import TailwindColorNameBar from "./components/color/tailwindColorNameBar.vue";
 </template>
 <style>
 .alumni-sans-pinstripe-regular {
-  font-family: "Alumni Sans Pinstripe", sans-serif;
-  font-weight: 400;
-  font-style: normal;
+font-family: "Alumni Sans Pinstripe", sans-serif;
+font-weight: 400;
+font-style: normal;
 }
 .alumni-sans-pinstripe-regular-italic {
-  font-family: "Alumni Sans Pinstripe", sans-serif;
-  font-weight: 400;
-  font-style: italic;
+font-family: "Alumni Sans Pinstripe", sans-serif;
+font-weight: 400;
+font-style: italic;
 }
 .coral-pixels-regular {
-  font-family: "Coral Pixels", serif;
-  font-weight: 400;
-  font-style: normal;
+font-family: "Coral Pixels", serif;
+font-weight: 400;
+font-style: normal;
 }
 .hubot-sans {
-  font-family: "Hubot Sans", sans-serif;
-  font-optical-sizing: auto;
-  font-weight: <weight>;
-  font-style: normal;
-  font-variation-settings: "wdth" 100;
+font-family: "Hubot Sans", sans-serif;
+font-optical-sizing: auto;
+font-weight: <weight>;
+font-style: normal;
+font-variation-settings: "wdth" 100;
 }
 .mea-culpa-regular {
-  font-family: "Mea Culpa", cursive;
-  font-weight: 400;
-  font-style: normal;
+font-family: "Mea Culpa", cursive;
+font-weight: 400;
+font-style: normal;
 }
 .orbitron {
-  font-family: "Orbitron", sans-serif;
-  font-optical-sizing: auto;
-  font-weight: <weight>;
-  font-style: normal;
+font-family: "Orbitron", sans-serif;
+font-optical-sizing: auto;
+font-weight: <weight>;
+font-style: normal;
 }
 .rajdhani-light {
-  font-family: "Rajdhani", sans-serif;
-  font-weight: 300;
-  font-style: normal;
+font-family: "Rajdhani", sans-serif;
+font-weight: 300;
+font-style: normal;
 }
 .rajdhani-regular {
-  font-family: "Rajdhani", sans-serif;
-  font-weight: 400;
-  font-style: normal;
+font-family: "Rajdhani", sans-serif;
+font-weight: 400;
+font-style: normal;
 }
 .rajdhani-medium {
-  font-family: "Rajdhani", sans-serif;
-  font-weight: 500;
-  font-style: normal;
+font-family: "Rajdhani", sans-serif;
+font-weight: 500;
+font-style: normal;
 }
 .rajdhani-semibold {
-  font-family: "Rajdhani", sans-serif;
-  font-weight: 600;
-  font-style: normal;
+font-family: "Rajdhani", sans-serif;
+font-weight: 600;
+font-style: normal;
 }
 .rajdhani-bold {
-  font-family: "Rajdhani", sans-serif;
-  font-weight: 700;
-  font-style: normal;
+font-family: "Rajdhani", sans-serif;
+font-weight: 700;
+font-style: normal;
 }
 
 .hue090 { color: hsl(90, 050%, 070%, 100%); }
--- src/components/color/backgroundImages.vue
+++ src/components/color/backgroundImages.vue
@@ -9,12 +9,7 @@ defineOptions({
     background-position: 0, 0px;
     background-repeat: repeat;
     background-size: auto;
-    
-    /*background-image: url('/color/30 hsl wheel-0red 10k opacity 10 - yellow.png'), url('/color/30 hsl wheel-0red 10k opacity 10 - yellow.png'), url('/NaughtSea.png');
-    background-position: 1 11 0, 33px 11px 0px;
-    background-repeat: repeat, repeat, repeat;
-    background-size: 333px, 1111px, auto;
-    */
+
   }
 </style>
 <template>
